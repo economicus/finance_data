92cf97768fad2dfbe78ed725d31f0602884ab20d
aLVINlEE9 <fldh3746@gmail.com>
2022-02-14 19:59:45 +0900

    none parameter issue fixing

--- quant/quant_calc.py
+++ quant/quant_calc.py
@@ -11,17 +11,20 @@ class QuantCalc(FindCode, Calculate):
 		self.ref_date = []
 	
 
+
+
 	def execute(self):
 		codes = self.apply_conditions(start_date=datetime(2018,1,1), end_date=None, \
-									term=6, market=None, main_sector=None, net_rev=None, \
-									net_rev_r=None, net_prf=None, de_r=None, per=None, psr=None, \
-									pbr=None, op_act=None, iv_act=None, fn_act=None, dv_yld=None, \
-									dv_pay_r=None, roa=None, roe=None, marcap=None, vol=None, \
-									pvol=None, sma5=None, sma20=None, sma60=None, sma120=None, \
-									ema5=None, ema20=None, ema60=None, ema120=None, past_prf_r1=None, \
-									past_prf_r3=None, past_prf_r6=None, past_prf_r12=None)
-		
-		self.calculate_profit(codes)
+									term=6, market="KOSPI", main_sector=None, net_rev=None, \
+									net_rev_r=None, net_prf=None, net_prf_r=None, de_r=None, \
+									per=[0, 10], psr=None, pbr=None, op_act=None, iv_act=None, \
+									fn_act=None, dv_yld=None, dv_pay_r=None, roa=None, roe=None, \
+									marcap=None, vol=None, pvol=None, sma5=None, sma20=None, \
+									sma60=None, sma120=None, ema5=None, ema20=None, ema60=None, \
+									ema120=None, past_prf_r1=None, past_prf_r3=None, \
+									past_prf_r6=None, past_prf_r12=None)
+		print(codes)
+		# self.calculate_profit(codes)
 
 if __name__ == "__main__":
 
