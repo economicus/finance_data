dda75af14db3e48ce75079e274085fb0567ce1b7
Wonjun Choi <78288539+jhon3242@users.noreply.github.com>
2022-02-23 18:25:24 +0900

    Delete connect.py

--- data_collector/connect.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy as db
-
-class SQLAlchemyConnector:
-	
-	def __init__(self):
-		self.engine = db.create_engine('mysql+pymysql://root:passwd@localhost/db_name')
-		self.connection = self.engine.connect()
-
