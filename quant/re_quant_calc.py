8ba632a447d4320f1aefc65f9fe915abaf3f1150
aLVINlEE9 <fldh3746@gmail.com>
2022-03-03 14:27:24 +0900

    x10 performance

--- quant/re_quant_calc.py
+++ quant/re_quant_calc.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta
 import swifter
 import time
@@ -26,13 +26,16 @@ class DataLoad:
 
 		fin_df.dropna(subset = ["구분"], inplace=True)
 		# quarter
-		fin_df["구분"] = fin_df["구분"].swifter.apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+		fin_df["구분"] = fin_df["구분"].swifter.apply(lambda x: datetime.timestamp(datetime.strptime(x, "%Y-%m-%d")))
 
-		pri_df["Date"] = pri_df["Date"].swifter.apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+		pri_df["Date"] = pri_df["Date"].swifter.apply(lambda x: datetime.timestamp(datetime.strptime(x, "%Y-%m-%d")))
 
 		com_np = com_df.to_numpy()
 		fin_np = fin_df.to_numpy()
 		pri_np = pri_df.to_numpy()
+
+		pri_np = np.float32(pri_np)
+		fin_np = np.float32(fin_np)
 		print("complete!")
 
 		return com_np, fin_np, pri_np
@@ -44,12 +47,10 @@ class FindCode(DataLoad):
 		self.ref_date = []
 	
 	def trunc_dt_31(self, someDate):
-		return datetime(someDate.year - 1, 12, 31, 0, 0, 0, 0)
+		return datetime.timestamp(datetime(someDate.year - 1, 12, 31, 0, 0, 0, 0))
 
 	# list of rebalacing term
 	def rebalacing(self, start, end):
-		if end == None:
-			end = datetime(2021, 3, 31, 0, 0, 0, 0, tzinfo=timezone.utc)
 		while(1):
 			temp = end
 			end -= relativedelta(years=1)
@@ -78,7 +79,7 @@ class FindCode(DataLoad):
 	def apply_each_finance(self, ref_date, fin_np, cond):
 
 		# ref_date filtered
-		fin_np = fin_np[fin_np[:,1] == self.trunc_dt_31(ref_date[0]).date()]
+		fin_np = fin_np[fin_np[:,1] == self.trunc_dt_31(ref_date[0])]
 
 		# numpy indexes
 		cond_idx = dict(net_rev=2, net_rev_r=3, net_prf=4, net_prf_r=5, de_r=6, \
@@ -98,7 +99,7 @@ class FindCode(DataLoad):
 		fin_np = fin_np[conditions]
 		
 		# return True False list of IDs
-		return [fin_np[:,0] , ref_date]
+		return ([fin_np[:,0] , ref_date])
 
 	def apply_finance(self, cond, conditions):
 		fin_np = self.fin_np
@@ -113,7 +114,7 @@ class FindCode(DataLoad):
 		self.rebalacing(cond["start_date"], cond["end_date"])
 		conditions = self.apply_company(cond["main_sector"])
 		conditions = self.apply_finance(cond, conditions)
-		return conditions[::-1]
+		return (conditions[::-1])
 
 
 class Calculate(DataLoad):
@@ -122,21 +123,21 @@ class Calculate(DataLoad):
 		self.prf = 0
 
 	def trunc_dt_401(self, someDate):
-		return datetime(someDate.year, someDate.month + 1, 1, 0, 0, 0, 0)
+		return datetime.timestamp(datetime(someDate.year, someDate.month + 1, 1, 0, 0, 0, 0))
 
 	def trunc_dt_430(self, someDate):
-		return datetime(someDate.year, someDate.month + 1, 30, 0, 0, 0, 0)
+		return datetime.timestamp(datetime(someDate.year, someDate.month + 1, 30, 0, 0, 0, 0))
 
 
 	def get_max_loss_rate(self, ret):
 		return ((1 + min(ret) / 100) / (1 + max(ret) / 100) - 1) * 100
 
 	def get_holdings_count(self, code_list):
-		return[len(c[0]) for c in code_list]
+		return([len(c[0]) for c in code_list])
 
 	def get_winning_percentage(self, ret):
 		wr = [True if ret[i+1] - ret[i] > 0 else False for i in range(len(ret)-1)]
-		return wr.count(True) / len(wr) * 100
+		return (wr.count(True) / len(wr) * 100)
 
 	def get_annual_average_return(self, ret):
 		av_c = [[r[i+1] - r[i] for i in range(len(r)-1)] for r in ret]
@@ -155,7 +156,7 @@ class Calculate(DataLoad):
 			profits.append(profit*100)
 		if len(profits) != 12:
 			return [0] * 12
-		return profits
+		return (profits)
 		
 
 	def calculate_each_term(self, codes, ref_date):
@@ -163,8 +164,8 @@ class Calculate(DataLoad):
 			return [self.last_profit * 100.0] * 12
 		profit = 0
 		pri_np = self.pri_np
-		pri_np=pri_np[(pri_np[:,1] >= self.trunc_dt_401(ref_date[0]).date()) & \
-						(pri_np[:,1] <= self.trunc_dt_430(ref_date[1]).date())]
+		pri_np=pri_np[(pri_np[:,1] >= self.trunc_dt_401(ref_date[0])) & \
+						(pri_np[:,1] <= self.trunc_dt_430(ref_date[1]))]
 		result = [self.calculate_each_code(c, pri_np)for c in codes]
 		result_np = np.array(result)
 		profit = result_np.sum(axis=0)
@@ -195,36 +196,39 @@ class QuantCalc(FindCode, Calculate):
 	def __init__(self):
 		super().__init__()
 
-
 	# execute all process
 	def execute(self, **cond):
 		# apply conditions to filter the stocks
 		code_list = self.apply_conditions(cond)
 		# put filtered stocks and calculate profits
 		return_dict = self.calculate_profit(code_list, cond)
+		print(return_dict)
 		return return_dict
 
 
 
 if __name__ == "__main__":
 	a = QuantCalc()
-	a.execute(start_date=datetime(2016, 3, 31, 0, 0, 0, 0, tzinfo=timezone.utc), 
+	start = time.time()
+	a.execute(start_date=datetime(2009, 3, 31, 0, 0, 0, 0, tzinfo=timezone.utc), 
 				end_date=datetime(2021, 3, 31, 0, 0, 0, 0, tzinfo=timezone.utc), 
-				main_sector=['IT'],
-				net_rev=dict(min=-10000, max=1000000),
+				main_sector=['IT', '산업재', '커뮤니케이션서비스'],
+				net_rev=dict(min=0, max=1000000000000),
 				net_rev_r=dict(min=None, max=None),
-				net_prf=dict(min=-100, max=100),
+				net_prf=dict(min=None, max=None),
 				net_prf_r=dict(min=None, max=None),
-				de_r=dict(min=-100.0, max=100.0),
-				per=dict(min=-100.0, max=100.0),
+				de_r=dict(min=None, max=None),
+				per=dict(min=None, max=None),
 				psr=dict(min=None, max=None),
-				pbr=dict(min=-100.0, max=100.0),
+				pbr=dict(min=None, max=None),
 				pcr=dict(min=None, max=None),
-				op_act=dict(min=-100.0, max=100.0),
-				iv_act=dict(min=-100.0, max=100.0),
-				fn_act=dict(min=-100.0, max=100.0),
-				dv_yld=dict(min=-100.0, max=100.0),
-				dv_pay_r=dict(min=-100.0, max=100.0),
-				roa=dict(min=-100.0, max=100.0),
-				roe=dict(min=-100.0, max=100.0),
-				marcap=dict(min=-100.0, max=100.0))
+				op_act=dict(min=None, max=None),
+				iv_act=dict(min=None, max=None),
+				fn_act=dict(min=None, max=None),
+				dv_yld=dict(min=None, max=None),
+				dv_pay_r=dict(min=None, max=None),
+				roa=dict(min=None, max=None),
+				roe=dict(min=None, max=None),
+				marcap=dict(min=None, max=None))
+	delta_t = time.time() - start
+	print("total Process : ",delta_t,"s")
