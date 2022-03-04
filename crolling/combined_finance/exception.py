abe8d6aee853d9a8df4a80e7b84b219e7f3c0964
Wonjun Choi <78288539+jhon3242@users.noreply.github.com>
2022-03-04 19:06:34 +0900

    모듈화 및 코드 수정
    예외처리 함수 모듈화를 통해 가독성을 늘리고 예외 처리를 추가하였다.

---
+++ crolling/combined_finance/exception.py
@@ -0,0 +1,124 @@
+import pandas as pd
+
+
+def except_for_git_make_same_columns(g_df):
+	""" 칼럼 순서가 바뀐 경우의 예외처리 함수 """
+	if g_df.iloc[:,1].name == 'DPS(보통주,현금+주식)':
+		col_0 = g_df.columns[:1].to_list()
+		col_1 = g_df.columns[38:].to_list()
+		col_2 = g_df.columns[28:38].to_list()
+		col_3 = g_df.columns[1:28].to_list()
+		new_col = col_0 + col_1 + col_2 + col_3
+		g_df = g_df[new_col]
+
+	elif g_df.iloc[:,13].name == '영업활동현금흐름':
+		if g_df.iloc[:,16].name == 'DPS(보통주,현금+주식)':
+			col_0 = g_df.columns[:13].to_list()
+			col_1 = g_df.columns[43:].to_list()
+			col_2 = g_df.columns[13:43].to_list()
+			new_col = col_0 + col_1 + col_2
+		else:
+			col_0 = g_df.columns[:13].to_list()
+			col_1 = g_df.columns[16:23].to_list()
+			col_2 = g_df.columns[13:16].to_list()
+			col_3 = g_df.columns[23:].to_list()
+			new_col = col_0 + col_1 + col_2 + col_3
+		g_df = g_df[new_col]
+
+	elif  g_df.iloc[:,13].name == 'DPS(보통주,현금+주식)':
+		col_1 = g_df.columns[:13].to_list()
+		col_2 =g_df.columns[40:].to_list()
+		col_3 =g_df.columns[13:40].to_list()
+		new_col=col_1+col_2 + col_3
+		g_df = g_df[new_col]
+
+	elif g_df.iloc[:,13].name != '매출액':
+		col_1 = g_df.columns[:13].to_list()
+		col_2 = g_df.columns[43:].to_list()
+		col_3 = g_df.columns[13:43].to_list()
+		new_col=col_1+col_2 + col_3
+		g_df = g_df[new_col]
+		
+	elif g_df.iloc[:,43].name == 'PCR':
+		print("@@@")
+		col_1 = g_df.columns[:20].to_list()
+		col_2 = g_df.columns[47:].to_list()
+		col_3 = g_df.columns[20:47].to_list()
+		new_col=col_1+col_2 + col_3
+		g_df = g_df[new_col]
+		
+	return g_df
+
+def except_kor(g_df):
+	""" 예외 케이스 처리 함수 """
+	kor = ['흑전', '적전', '적지', '완전잠식']
+	g_df['매출액증가율(전년동기)'] = g_df['매출액증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
+	g_df['영업이익증가율(전년동기)'] = g_df['영업이익증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
+	g_df['당기순이익증가율(전년동기)'] = g_df['당기순이익증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
+	g_df['부채비율'] = g_df['부채비율'].apply(lambda x : 0 if x in kor else x)
+	g_df['ROE(영업이익)'] = g_df['ROE(영업이익)'].apply(lambda x : 0 if x in kor else x)
+	g_df['ROE(당기순이익)'] = g_df['ROE(당기순이익)'].apply(lambda x : 0 if x in kor else x)
+	return g_df
+
+def except_KONEX_list(comp_id_path):
+	df = pd.read_csv(comp_id_path)
+	KONEX_list = []
+	for i in df[df['Market'] == 'KONEX']['Symbol']:
+		KONEX_list.append('{:0>6}'.format(i))
+	return KONEX_list
+
+
+def extract_from_naver(i, n_df):
+	""" 네이버 재무제표에서 깃 재무제표 컬럼에 있는 요소들을 추출하는 함수 """
+	data = []
+	data.append(n_df.loc[0, i].split('/')[0]) # 구분
+	data.append(int(n_df.loc[8, i].replace(',', '') + '00000')) # 자산
+	data.append(None) # 유동자산
+	data.append(None) # 비유동자산
+	data.append(None) # 기타자산
+	data.append(int(n_df.loc[9, i].replace(',', '') + '00000')) # 부채
+	data.append(None) # 유동부채
+	data.append(None) # 비유동부채
+	data.append(int(n_df.loc[10, i].replace(',', '') + '00000')) # 자본
+	data.append(None) # 주당액면가액
+	data.append(int(n_df.loc[33, i].replace(',', ''))) # 발행주식수
+	data.append(None) # 보통주
+	data.append(None) # 우선주
+	data.append(int(n_df.loc[1, i].replace(',', '') + '00000')) # 매출액
+	data.append(None) # 매출원가
+	data.append(None) # 매출총이익
+	data.append(None) # 판매비와관리비
+	data.append(int(n_df.loc[2, i].replace(',', '') + '00000')) # 영업이익
+	data.append(None) # 법인세차감전순이익
+	data.append(int(n_df.loc[5, i].replace(',', '') + '00000')) # 당기순이익
+	data.append(int(n_df.loc[14, i].replace(',', '') + '00000')) # 영업활동현금흐름
+	data.append(int(n_df.loc[15, i].replace(',', '') + '00000')) # 투자활동현금흐름
+	data.append(int(n_df.loc[16, i].replace(',', '') + '00000')) # 재무활동현금흐름
+	data.append(int(n_df.loc[30, i].replace(',', ''))) # DPS
+	data.append(None) # 배당금
+	data.append(float(n_df.loc[32, i].replace(',', ''))) # 배당성향
+	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[1, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[1, i].replace(',', '') + '00000') != 0 else 0 ) # 영업이익률
+	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[1, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[1, i].replace(',', '') + '00000') != 0 else 0 ) # 당기순이익률
+	data.append(round(int(n_df.loc[1, i].replace(',', '') + '00000') / int(n_df.loc[1, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and int(n_df.loc[1, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 매출액증가율
+	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[2, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and  int(n_df.loc[2, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 영업이익증가율
+	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[5, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and  int(n_df.loc[5, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 당기순이익증가율
+	data.append(None) # 이자보상배율
+	data.append(None) # 유동비율
+	data.append(round(int(n_df.loc[9, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # 부채비율
+	data.append(n_df.loc[31, i]) # 배당수익률
+	data.append(None) # 매출총이익률
+	data.append(round(int(n_df.loc[1, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000'), 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # 총자산회전율
+	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[10, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[10, i].replace(',', '') + '00000') != 0 else 0) # ROE 영업이익
+	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[10, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[10, i].replace(',', '') + '00000') != 0 else 0) # ROE 당기순이익
+	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # ROA 영업이익
+	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # ROA 당기순이익
+	data.append(int(n_df.loc[26, i].replace(',', ''))) # EPS
+	data.append(int(n_df.loc[28, i].replace(',', ''))) # BPS
+	data.append(float(n_df.loc[27, i].replace(',', ''))) # PER
+	data.append(float(n_df.loc[29, i].replace(',', ''))) # PBR
+	data.append(None) # PSR
+	data.append(None) # PCR
+	data.append(None) # ROIC
+	data.append(None) # EV/EBIT
+	data.append(None) # EV/EBITDA
+	return (data)
--- crolling/combined_finance/extract.py
+++ crolling/combined_finance/extract.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import time
-
-
+from exception import *
 
 def read_code_csv(path):
 	df = pd.read_csv(path)
@@ -18,127 +17,29 @@ def id_to_code(codes, path):
 		code_list.append(int(code))
 	return code_list
 	
-def except_for_git_make_same_columns(g_df):
-	""" 칼럼 순서가 바뀐 경우의 예외처리 함수 """
-	if g_df.iloc[:,1].name == 'DPS(보통주,현금+주식)':
-		col_0 = g_df.columns[:1].to_list()
-		col_1 = g_df.columns[38:].to_list()
-		col_2 = g_df.columns[28:38].to_list()
-		col_3 = g_df.columns[1:28].to_list()
-		new_col = col_0 + col_1 + col_2 + col_3
-		g_df = g_df[new_col]
 
-	elif g_df.iloc[:,13].name == '영업활동현금흐름':
-		if g_df.iloc[:,16].name == 'DPS(보통주,현금+주식)':
-			col_0 = g_df.columns[:13].to_list()
-			col_1 = g_df.columns[43:].to_list()
-			col_2 = g_df.columns[13:43].to_list()
-			new_col = col_0 + col_1 + col_2
-		else:
-			col_0 = g_df.columns[:13].to_list()
-			col_1 = g_df.columns[16:23].to_list()
-			col_2 = g_df.columns[13:16].to_list()
-			col_3 = g_df.columns[23:].to_list()
-			new_col = col_0 + col_1 + col_2 + col_3
-		g_df = g_df[new_col]
-
-	elif  g_df.iloc[:,13].name == 'DPS(보통주,현금+주식)':
-		col_1 = g_df.columns[:13].to_list()
-		col_2 =g_df.columns[40:].to_list()
-		col_3 =g_df.columns[13:40].to_list()
-		new_col=col_1+col_2 + col_3
-		g_df = g_df[new_col]
-
-	elif g_df.iloc[:,13].name != '매출액':
-		col_1 = g_df.columns[:13].to_list()
-		col_2 = g_df.columns[43:].to_list()
-		col_3 = g_df.columns[13:43].to_list()
-		new_col=col_1+col_2 + col_3
-		g_df = g_df[new_col]
-		
-	elif g_df.iloc[:,43].name == 'PCR':
-		print("@@@")
-		col_1 = g_df.columns[:20].to_list()
-		col_2 = g_df.columns[47:].to_list()
-		col_3 = g_df.columns[20:47].to_list()
-		new_col=col_1+col_2 + col_3
-		g_df = g_df[new_col]
-		
-	return g_df
-
-def except_kor(g_df):
-	""" 예외 케이스 처리 함수 """
-	kor = ['흑전', '적전', '적지', '완전잠식']
-	g_df['매출액증가율(전년동기)'] = g_df['매출액증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
-	g_df['영업이익증가율(전년동기)'] = g_df['영업이익증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
-	g_df['당기순이익증가율(전년동기)'] = g_df['당기순이익증가율(전년동기)'].apply(lambda x : 0 if x in kor else x)
-	g_df['부채비율'] = g_df['부채비율'].apply(lambda x : 0 if x in kor else x)
-	g_df['ROE(영업이익)'] = g_df['ROE(영업이익)'].apply(lambda x : 0 if x in kor else x)
-	g_df['ROE(당기순이익)'] = g_df['ROE(당기순이익)'].apply(lambda x : 0 if x in kor else x)
-	return g_df
-
-def except_KONEX_list(comp_id_path):
-	df = pd.read_csv(comp_id_path)
-	KONEX_list = []
-	for i in df[df['Market'] == 'KONEX']['Symbol']:
-		KONEX_list.append('{:0>6}'.format(i))
-	return KONEX_list
+def for_make_yyyy_mm_dd(x):
+	if pd.isna(x) or x == 'nan' or x == None:
+		return None
+	if '.0' in x:
+		x.replace('.0', '')
+	return x + '-12-31'
 
 
-def extract_from_naver(i, n_df):
-	""" 네이버 재무제표에서 깃 재무제표 컬럼에 있는 요소들을 추출하는 함수 """
-	data = []
-	data.append(n_df.loc[0, i].split('/')[0]) # 구분
-	data.append(int(n_df.loc[8, i].replace(',', '') + '00000')) # 자산
-	data.append(None) # 유동자산
-	data.append(None) # 비유동자산
-	data.append(None) # 기타자산
-	data.append(int(n_df.loc[9, i].replace(',', '') + '00000')) # 부채
-	data.append(None) # 유동부채
-	data.append(None) # 비유동부채
-	data.append(int(n_df.loc[10, i].replace(',', '') + '00000')) # 자본
-	data.append(None) # 주당액면가액
-	data.append(int(n_df.loc[33, i].replace(',', ''))) # 발행주식수
-	data.append(None) # 보통주
-	data.append(None) # 우선주
-	data.append(int(n_df.loc[1, i].replace(',', '') + '00000')) # 매출액
-	data.append(None) # 매출원가
-	data.append(None) # 매출총이익
-	data.append(None) # 판매비와관리비
-	data.append(int(n_df.loc[2, i].replace(',', '') + '00000')) # 영업이익
-	data.append(None) # 법인세차감전순이익
-	data.append(int(n_df.loc[5, i].replace(',', '') + '00000')) # 당기순이익
-	data.append(int(n_df.loc[14, i].replace(',', '') + '00000')) # 영업활동현금흐름
-	data.append(int(n_df.loc[15, i].replace(',', '') + '00000')) # 투자활동현금흐름
-	data.append(int(n_df.loc[16, i].replace(',', '') + '00000')) # 재무활동현금흐름
-	data.append(int(n_df.loc[30, i].replace(',', ''))) # DPS
-	data.append(None) # 배당금
-	data.append(float(n_df.loc[32, i].replace(',', ''))) # 배당성향
-	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[1, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[1, i].replace(',', '') + '00000') != 0 else 0 ) # 영업이익률
-	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[1, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[1, i].replace(',', '') + '00000') != 0 else 0 ) # 당기순이익률
-	data.append(round(int(n_df.loc[1, i].replace(',', '') + '00000') / int(n_df.loc[1, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and int(n_df.loc[1, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 매출액증가율
-	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[2, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and  int(n_df.loc[2, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 영업이익증가율
-	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[5, str(int(i) - 1)].replace(',', '') + '00000') * 100 - 100, 2) if int(i) != 1 and  int(n_df.loc[5, str(int(i) - 1)].replace(',', '') + '00000') != 0 else 0) # 당기순이익증가율
-	data.append(None) # 이자보상배율
-	data.append(None) # 유동비율
-	data.append(round(int(n_df.loc[9, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # 부채비율
-	data.append(n_df.loc[31, i]) # 배당수익률
-	data.append(None) # 매출총이익률
-	data.append(round(int(n_df.loc[1, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000'), 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # 총자산회전율
-	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[10, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[10, i].replace(',', '') + '00000') != 0 else 0) # ROE 영업이익
-	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[10, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[10, i].replace(',', '') + '00000') != 0 else 0) # ROE 당기순이익
-	data.append(round(int(n_df.loc[2, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # ROA 영업이익
-	data.append(round(int(n_df.loc[5, i].replace(',', '') + '00000') / int(n_df.loc[8, i].replace(',', '') + '00000') * 100, 2) if int(n_df.loc[8, i].replace(',', '') + '00000') != 0 else 0) # ROA 당기순이익
-	data.append(int(n_df.loc[26, i].replace(',', ''))) # EPS
-	data.append(int(n_df.loc[28, i].replace(',', ''))) # BPS
-	data.append(float(n_df.loc[27, i].replace(',', ''))) # PER
-	data.append(float(n_df.loc[29, i].replace(',', ''))) # PBR
-	data.append(None) # PSR
-	data.append(None) # PCR
-	data.append(None) # ROIC
-	data.append(None) # EV/EBIT
-	data.append(None) # EV/EBITDA
-	return (data)
+def union_structure(save_path):
+	""" csv 구조를 통일하는 함수 """
+	codes = read_code_csv('/Users/choewonjun/Documents/coding/crolling')
+	for code in codes:
+		code = '{:0>6}'.format(code)
+		print(code)
+		save_path = '/Users/choewonjun/Documents/coding/crolling/finished'
+		df = pd.read_csv(f'{save_path}/{code}.csv', encoding='cp949')
+		if 'Unnamed: 0' in df.columns:
+			l = list(df.columns)
+			splice_idx = l.index('Unnamed: 0')
+			new_df = df.iloc[:,splice_idx + 1:]
+			new_df.to_csv(f'{save_path}/{code}.csv', encoding='cp949', index=False)
+
 
 
 def combine_git_naver(naver_path, git_path, comp_id_path, save_path):
@@ -146,14 +47,15 @@ def combine_git_naver(naver_path, git_path, comp_id_path, save_path):
 	codes, _  = read_code_csv(comp_id_path)
 	count = 0
 	KONEX_list = except_KONEX_list(comp_id_path)
-	no_december = ['169330', '190650' ,'357120', '950210', '338100', '365550']
+	no_december = ['169330', '190650' ,'357120', '950210', '338100', '365550', '334890'] # 실적발표가 12월이 아닌 회사
+	no_report = ['402340', '383310', '378850', '383800'] # 재무제표가 없는 회사
 	# 코넥스가 들어오는 경우 예외처리 필요
 	for code in codes:
 		code = '{:0>6}'.format(code)
 		# if code != '078130':
 		# 	continue
 
-		if code == '334890' or code == '402340' or code in KONEX_list: # 예외 케이스
+		if code in no_report or code in KONEX_list: # 예외 케이스
 			print('Exception case')
 			continue
 		
@@ -173,10 +75,9 @@ def combine_git_naver(naver_path, git_path, comp_id_path, save_path):
 					datas.append(data)
 					count += 1
 					continue			
-				# print(n_df.loc[0, i])
 
 				if int(n_df.loc[0, i].split('/')[0]) <= 2018 or int(n_df.loc[0, i].split('/')[1][:2]) != 12 or 'E' in n_df.loc[0, i]:
-					if code in no_december: # 예외처리 (실적이 12월에 안나는 회사)
+					if code in no_december and 'E' not in n_df.loc[0, i]: # 예외처리 (실적이 12월에 안나는 회사)
 						pass
 					else:
 						continue
@@ -221,29 +122,6 @@ def combine_git_naver(naver_path, git_path, comp_id_path, save_path):
 		# return
 		csv_df.to_csv(f'{save_path}/{code}.csv', encoding='cp949', index=False)
 		# csv_df.to_csv(f'/Users/choewonjun/Desktop/ck/{code}.csv', encoding='cp949')
-
-
-def union_structure(save_path):
-	""" csv 구조를 통일하는 함수 """
-	codes = read_code_csv('/Users/choewonjun/Documents/coding/crolling')
-	for code in codes:
-		code = '{:0>6}'.format(code)
-		print(code)
-		save_path = '/Users/choewonjun/Documents/coding/crolling/finished'
-		df = pd.read_csv(f'{save_path}/{code}.csv', encoding='cp949')
-		if 'Unnamed: 0' in df.columns:
-			l = list(df.columns)
-			splice_idx = l.index('Unnamed: 0')
-			new_df = df.iloc[:,splice_idx + 1:]
-			new_df.to_csv(f'{save_path}/{code}.csv', encoding='cp949', index=False)
-
-
-def for_make_yyyy_mm_dd(x):
-	if pd.isna(x) or x == 'nan' or x == None:
-		return None
-	if '.0' in x:
-		x.replace('.0', '')
-	return x + '-12-31'
 
 
 def make_one_csv(comp_id_path, save_path):
@@ -305,7 +183,7 @@ if __name__ == "__main__":
 
 	# union_structure(save_path)
 
-	combine_git_naver(naver_path, git_path, comp_id_path, save_path)
+	# combine_git_naver(naver_path, git_path, comp_id_path, save_path)
 	make_one_csv(comp_id_path, save_path)
 
 	# end_time = time.time()
